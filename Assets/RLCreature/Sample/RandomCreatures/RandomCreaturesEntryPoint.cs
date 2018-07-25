91d44d84ad1cf9c03d34259685cc1c72c0e02dc6
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2018-07-25 17:48:05 +0900

    :star: random creature

--- Assets/RLCreature/Sample/RandomCreatures/RandomCreaturesEntryPoint.cs
+++ Assets/RLCreature/Sample/RandomCreatures/RandomCreaturesEntryPoint.cs
@@ -1,4 +1,5 @@
-﻿using MotionGenerator;
+﻿using System.Collections;
+using MotionGenerator;
 using RLCreature.BodyGenerator;
 using RLCreature.BodyGenerator.JointGenerator;
 using RLCreature.BodyGenerator.Manipulatables;
@@ -43,60 +44,36 @@ namespace RLCreature.Sample.RandomCreatures
             );
             Agent.CreateComponent(centralBody, brain, new Body(centralBody), actions);
 
-
-//            StartCoroutine(Feeder());
-////            StartCoroutine(SpawnSome());
-//            var creature = StartCreature(CreatureRootGameObject, CentralBody);
-//            var camera = Camera.main;
-//            camera.transform.parent = creature.transform;
-//            camera.transform.position = creature.transform.position - creature.transform.forward * 30 +
-//                                        creature.transform.up * 10;
+            StartCoroutine(Feeder());
         }
 
 
-//        private GameObject StartCreature(GameObject creatureRootGameObject, GameObject centralBody)
-//        {
-//            Sensor.CreateComponent(centralBody, typeof(Food), State.BasicKeys.RelativeFoodPosition, range: 100f);
-//            Mouth.CreateComponent(centralBody, typeof(Food));
-//
-//            var actions = LocomotionAction.EightDirections();
-//            var sequenceMaker = new EvolutionarySequenceMaker(epsilon: 0.3f, minimumCandidates: 30);
-//            var brain = new Brain(
-//                new FollowPointDecisionMaker(State.BasicKeys.RelativeFoodPosition),
-//                sequenceMaker
-//            );
-//            Agent.CreateComponent(creatureRootGameObject, brain, new Body(centralBody), actions);
-//
-//            return centralBody;
-//        }
-//
-//
-//        private IEnumerator Feeder()
-//        {
-//            while (true)
-//            {
-//                var foodCount = FindObjectsOfType<Food>().Length;
-//                for (int i = 0; i < FoodCount - foodCount; i++)
-//                {
-//                    Feed();
-//                }
-//
-//                yield return new WaitForSeconds(5);
-//            }
-//        }
-//
-//        private void Feed()
-//        {
-//            var foodObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-//            foodObject.transform.localScale = Vector3.one * 5;
-//            var food = foodObject.AddComponent<Food>();
-//            food.GetComponent<Renderer>().material.color = Color.green;
-//            food.GetComponent<Collider>().isTrigger = true;
-//            food.transform.position = new Vector3(
-//                x: _size.xMin + Random.value * _size.width,
-//                y: 0,
-//                z: _size.yMin + Random.value * _size.height
-//            );
-//        }
+        private IEnumerator Feeder()
+        {
+            while (true)
+            {
+                var foodCount = FindObjectsOfType<Food>().Length;
+                for (int i = 0; i < FoodCount - foodCount; i++)
+                {
+                    Feed();
+                }
+
+                yield return new WaitForSeconds(5);
+            }
+        }
+
+        private void Feed()
+        {
+            var foodObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            foodObject.transform.localScale = Vector3.one * 5;
+            var food = foodObject.AddComponent<Food>();
+            food.GetComponent<Renderer>().material.color = Color.green;
+            food.GetComponent<Collider>().isTrigger = true;
+            food.transform.position = new Vector3(
+                x: _size.xMin + Random.value * _size.width,
+                y: 0,
+                z: _size.yMin + Random.value * _size.height
+            );
+        }
     }
 }

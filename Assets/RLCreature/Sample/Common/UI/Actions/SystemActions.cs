1e15b0ea0955131f62b0dec7d4775174c30ae9b7
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2018-08-05 02:25:22 +0900

    :art: SystemActions

---
+++ Assets/RLCreature/Sample/Common/UI/Actions/SystemActions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RLCreature.Sample.Common.UI.UIComponents;
+using UnityEngine;
+
+namespace RLCreature.Sample.Common.UI.Actions
+{
+    public class SystemActions : IToolBarActions
+    {
+        public SystemActions()
+        {
+        }
+
+        public string GetCategory()
+        {
+            return "System";
+        }
+
+        public Dictionary<string, Action> GetActions()
+        {
+            return new Dictionary<string, Action>
+            {
+                {
+                    "TimeScale x1", () => { Time.timeScale = 1; }
+                },
+                {
+                    "TimeScale x2", () => { Time.timeScale = 2; }
+                },
+                {
+                    "TimeScale x5", () => { Time.timeScale = 5; }
+                },
+                {
+                    "TimeScale x10", () => { Time.timeScale = 10; }
+                }
+            };
+        }
+    }
+}
